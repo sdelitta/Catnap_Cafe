f94ee5a5fd0f62dafd2fbf0d749ea7c9a29124b4
Steven DeLitta <sdelitta@gmail.com>
2024-11-02 12:13:17 -0400

    Initial setup with React, Bootstrap, and basic structure

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Header from './components/Header';
+import Home from './pages/Home';
+import About from './pages/About';
+import Adopt from './pages/Adopt';
+
+function App() {
+    return (
+        <Router>
+            <Header />
+            <Routes>
+                <Route path="/" element={<Home />} />
+                <Route path="/about" element={<About />} />
+                <Route path="/adopt" element={<Adopt />} />
+            </Routes>
+        </Router>
+    );
+}
+
+export default App;
---
+++ src/components/Header.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import { Navbar, Nav, Container } from 'react-bootstrap';
+
+function Header() {
+    return (
+        <Navbar bg="light" expand="lg">
+            <Container>
+                <Navbar.Brand href="/">Catnap Cafe & Rescue</Navbar.Brand>
+                <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                <Navbar.Collapse id="basic-navbar-nav">
+                    <Nav className="me-auto">
+                        <Nav.Link href="/">Home</Nav.Link>
+                        <Nav.Link href="/about">About</Nav.Link>
+                        <Nav.Link href="/adopt">Adopt</Nav.Link>
+                    </Nav>
+                </Navbar.Collapse>
+            </Container>
+        </Navbar>
+    );
+}
+
+export default Header;
